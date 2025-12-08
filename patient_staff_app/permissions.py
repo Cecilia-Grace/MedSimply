e17611bb1d69fa2855179dbdac9a34dfd7290d98
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-08 16:29:48 +0300

    updated permissions

--- patient_staff_app/permissions.py
+++ patient_staff_app/permissions.py
@@ -3,6 +3,21 @@ from rest_framework import permissions
 class IsHealthWorker(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
-        return user.is_authenticated and user.groups.filter(name='HealthWorkers').exists()
         
+        if user.is_superuser:
+            return True
+        
+        return user.is_authenticated and user.groups.filter(name='HealthWorkers').exists()
+    
+    
+class CanViewPatients(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        
+        if user.is_superuser:
+            return True
+        
+        return user.is_authenticated and user.groups.filter(name='HealthWorkers').exists()
+    
+
         
