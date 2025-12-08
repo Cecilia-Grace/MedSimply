57b3777b9f51558abdcac84f2081261d6b13cb31
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-08 16:31:33 +0300

    fixed urls pattern bug for login history

--- patient_staff_app/urls.py
+++ patient_staff_app/urls.py
@@ -1,11 +1,11 @@
-from .views import PatientAssignmentViewSet, PatientViewSet, HealthWorkerViewSet
+from .views import PatientAssignmentViewSet, PatientViewSet, HealthWorkerViewSet, LoginHistoryViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'patients', PatientViewSet, basename='patient')
 router.register(r'health-workers', HealthWorkerViewSet, basename='health-worker')
 router.register(r'patient-assignments', PatientAssignmentViewSet, basename='patient-assignments')
-router.register(r'login-history', HealthWorkerViewSet, basename='login-history')
+router.register(r'login-history', LoginHistoryViewSet, basename='login-history')
 
 urlpatterns = router.urls
 
