333d0a45868519fd9271f307bdaa9c73151e56b0
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-10 00:31:28 +0300

    added viewsets

--- scheduling_app/views.py
+++ scheduling_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from .models import MedicationSchedule
-from .serializer import MedicationScheduleSerializer
+from .models import MedicationSchedule, NotificationDashboard
+from .serializer import MedicationScheduleSerializer, NotificationDashboardSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from django.utils import timezone
-from rest_framework.exceptions import PermissionDenied
 
 
 # Create your views here.
@@ -14,28 +13,8 @@ class MedicationScheduleViewSet(viewsets.ModelViewSet):
     
     permission_classes = [IsAdminUser]
     
-    def perform_update(self, serializer):
-        """
-        Automatically set time_given and given_by when marking as given,
-        and ensure only the assigned health worker can do it.
-        """
-        instance = serializer.instance
-        if serializer.validated_data.get('is_given'):
-            assignment = instance.patient_given_to.health_worker_assigned_to.filter(
-                end_time_date__isnull=True,
-                health_worker__user=self.request.user  # logged-in user must match
-            ).first()
-
-            if assignment:
-                serializer.save(
-                    time_given=timezone.now(),
-                    given_by=assignment
-                )
-            else:
-                # Deny if the logged-in user is not the assigned health worker
-                raise PermissionDenied("You are not assigned to this patient or dose.")
-        else:
-            serializer.save()  # Normal update if is_given is False
-     
-
-            
+class NotificationDashboardViewSet(viewsets.ModelViewSet):
+    queryset = NotificationDashboard.objects.all()
+    serializer_class = NotificationDashboardSerializer
+    
+    permission_classes = [IsAdminUser]
