d8f5390c5599692445588cfc5954ad57a8ba1efa
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-09 17:42:58 +0300

    commit

--- medication_inventory_app/views.py
+++ medication_inventory_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Medication, PatientMedication
+from rest_framework import viewsets
+from .serializer import MedicationSerializer, PatientMedicationSerializer 
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
 
 # Create your views here.
+class MedicationViewSet(viewsets.ModelViewSet):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+    
+    permission_classes = [IsAdminUser]
+    
+    
+class PatientMedicationViewSet(viewsets.ModelViewSet):
+    queryset = PatientMedication.objects.all()
+    serializer_class = PatientMedicationSerializer
+    
+    permission_classes = [IsAdminUser]
