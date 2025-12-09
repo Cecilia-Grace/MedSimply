0d4cffd6620ef1642e33937f250e950758dc865f
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-09 20:53:38 +0300

    registered views to djanjo site

--- scheduling_app/admin.py
+++ scheduling_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MedicationSchedule
 
 # Register your models here.
+@admin.register(MedicationSchedule)
+class MedicationScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'medication_given', 'patient_given_to', 'scheduled_time', 'is_given', 'given_by', 'time_given')
+    search_fields = ('id', 'medication_given', 'patient_given_to', 'scheduled_time', 'is_given', 'given_by', 'time_given')
