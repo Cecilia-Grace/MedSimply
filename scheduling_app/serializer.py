c724e782d8cb73fc62747970be40b6076140e063
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-10 00:30:30 +0300

    added serializer for notification model

--- scheduling_app/serializer.py
+++ scheduling_app/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MedicationSchedule
+from .models import MedicationSchedule, NotificationDashboard
 from django.contrib.auth.models import User
 
 class MedicationScheduleSerializer(serializers.ModelSerializer):
@@ -11,4 +11,9 @@ class MedicationScheduleSerializer(serializers.ModelSerializer):
         fields = ('id', 'medication_given', 'medication_given_display', 'patient_given_to', 'patient_given_to_display', 'scheduled_time')
         read_only_fields = ('medication_given', 'patient_given_to', 'scheduled_time')
         
-    
+class NotificationDashboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationDashboard
+        fields = ('id', 'patient_name', 'health_worker', 'phone_number', 'is_remainder_sent', 'remainder_response', 'remainder_message', 'is_given')
+        read_only_fields = ('id', 'patient_name', 'health_worker', 'phone_number', 'is_remainder_sent', 'remainder_response', 'remainder_message', 'is_given')
+   
