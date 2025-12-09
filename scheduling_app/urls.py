5b25ab64c7cfe4275ec69d073441a850bb4a46eb
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-10 00:31:11 +0300

    added urls for notification model

--- scheduling_app/urls.py
+++ scheduling_app/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import MedicationScheduleViewSet
+from .views import MedicationScheduleViewSet, NotificationDashboardViewSet
 
 router = DefaultRouter()
 router.register(r'medication_schedule', MedicationScheduleViewSet, basename='medication_schedules')
+router.register(r'notification_dashboard', NotificationDashboardViewSet, basename='notifications_dashboard')
 
 urlpatterns = router.urls
