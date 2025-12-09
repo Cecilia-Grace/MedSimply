2e43bb2e87f5f7170dbfe7489da9962487eaf418
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-09 22:33:20 +0300

    fixing api root bug

--- scheduling_app/serializer.py
+++ scheduling_app/serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import MedicationSchedule
+from django.contrib.auth.models import User
 
 class MedicationScheduleSerializer(serializers.ModelSerializer):
+    medication_given_display = serializers.CharField(source='medication_given.medication_name', read_only=True)
+    patient_given_to_display = serializers.CharField(source='patient_given_to.patient_name', read_only=True)
+    given_by_full_name = serializers.CharField(source='given_by.get_full_name()', read_only=True)
+    
     class Meta:
         model = MedicationSchedule
-        fields = ('id', 'medication_given', 'patient_given_to', 'scheduled_time', 'is_given', 'given_by', 'time_given')
+        fields = ('id', 'medication_given', 'medication_given_display', 'patient_given_to', 'patient_given_to_display', 'scheduled_time', 'is_given', 'given_by', 'given_by_full_name', 'time_given')
         read_only_fields = ('medication_given', 'patient_given_to', 'scheduled_time', 'is_given', 'given_by', 'time_given')
+        
+    
--- scheduling_app/views.py
+++ scheduling_app/views.py
@@ -4,6 +4,8 @@ from .serializer import MedicationScheduleSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from django.utils import timezone
+from rest_framework.exceptions import PermissionDenied
+
 
 # Create your views here.
 class MedicationScheduleViewSet(viewsets.ModelViewSet):
@@ -13,12 +15,27 @@ class MedicationScheduleViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
     
     def perform_update(self, serializer):
-        if 'is_given' in serializer.validated_data and serializer.validated_data['is_given']:
-            assignment = serializer.instance.patient_name.health_worker_assigned_to.filter(end_time_date__isnull=True).first()
-        
-            serializer.save(time_given=timezone.now(), given_by=assignment)
+        """
+        Automatically set time_given and given_by when marking as given,
+        and ensure only the assigned health worker can do it.
+        """
+        instance = serializer.instance
+        if serializer.validated_data.get('is_given'):
+            assignment = instance.patient_given_to.health_worker_assigned_to.filter(
+                end_time_date__isnull=True,
+                health_worker__user=self.request.user  # logged-in user must match
+            ).first()
+
+            if assignment:
+                serializer.save(
+                    time_given=timezone.now(),
+                    given_by=assignment
+                )
+            else:
+                # Deny if the logged-in user is not the assigned health worker
+                raise PermissionDenied("You are not assigned to this patient or dose.")
         else:
-            serializer.save()
+            serializer.save()  # Normal update if is_given is False
      
 
             
