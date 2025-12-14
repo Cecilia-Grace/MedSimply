f90e44a4727de7150ea5e47caec9700b064335fd
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-14 20:54:02 +0300

    fixed a bug

--- scheduling_app/utils.py
+++ scheduling_app/utils.py
@@ -1,21 +1,24 @@
 import africastalking
 
-#Africa's talking: notification sending
+#Africa's talking: notification sending(sandbox)
 AT_USERNAME = 'sandbox'
 AT_API_KEY = 'atsk_f3ea648d3b29e45c797ca0e41c94000956ae567ef38f4d1bf0bde8280c61b83d022234ec'
+
+# Africa's talking: notification sending(live)
+# AT_USERNAME = 'MedSimply'
+# AT_API_KEY = 'atsk_654376d04c657e07c5ba21f4ba42fb1543a5133990bdf3a84579f057a08bb4822107dae0'
 
 # Initialize Africa's Talking SDK (once)
 africastalking.initialize(AT_USERNAME, AT_API_KEY)
 sms = africastalking.SMS
 
-def send_sms(recipient_number: str, text_message: str):
+def send_sms(to, message):
     try:
         response = sms.send(
-            message=text_message,
-            recipients=[recipient_number],
-            sender_id="MedSimply Reminder"
+            message=message,
+            recipients=[to],
+            sender_id='MedSimply Reminder'
         )
         return response
     except Exception as e:
-        return f"Error sending SMS: {e}"
-
+        return f"SMS ERROR: {e}"
