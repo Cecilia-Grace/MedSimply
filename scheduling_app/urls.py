66634de43ce82b004077caa7129c2dd4b94cda94
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-14 20:52:34 +0300

    updated code for API

--- scheduling_app/urls.py
+++ scheduling_app/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import MedicationScheduleViewSet, NotificationDashboardViewSet
+from .views import MedicationScheduleViewSet, NotificationDashboardViewSet, receive_sms
+from django.contrib import admin
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'medication_schedule', MedicationScheduleViewSet, basename='medication_schedules')
 router.register(r'notification_dashboard', NotificationDashboardViewSet, basename='notifications_dashboard')
 
 urlpatterns = router.urls
+
+
