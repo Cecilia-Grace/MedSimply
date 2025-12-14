28830fdc861ae4d12e02a20f41341743705193a1
Cecilia Grace <cecigrace2003@gmail.com>
2025-12-14 20:56:14 +0300

    updated celery for automatic sms

--- MedSimply/celery.py
+++ MedSimply/celery.py
@@ -1,8 +1,10 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MedSimply.settings')
 
-app = Celery('project_name')
+app = Celery('MedSimply')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
 app.autodiscover_tasks()
